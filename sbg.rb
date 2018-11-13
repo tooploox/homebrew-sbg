e0a2c01d7520566e1ae6ba2d83fa5abcbeec5999
Karolina Samorek <skarol@users.noreply.github.com>
2018-11-13 09:14:58 +0100

    Fix patch to archive

--- sbg.rb
+++ sbg.rb
@@ -1,7 +1,7 @@
 class Sbg < Formula
   desc "A command line tool to generate swift boilerplate"
   homepage "https://github.com/tooploox/sbg"
-  url "https://github.com/tooploox/sbg/raw/master//Users/vagrant/git/source/archive/sbg-0.1.7.tar.gz"
+  url "https://github.com/tooploox/sbg/raw/master/archive/sbg-0.1.7.tar.gz"
   sha256 "26d706d54d9f9f4f20588a3ca4f2bc37be12233b05dafd59f7b86d9074347dc2"
   version "0.1.7"
 
